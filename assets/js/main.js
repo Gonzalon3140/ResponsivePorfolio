edc4de8337e37a01584a7088de24c6bca6ed4af6
Gonzalon <40928276+Gonzalon3140@users.noreply.github.com>
2019-06-10 10:07:08 -0700

    updated links and jquery have most animation working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
-$(document).ready(function() {
-  $(".menu-toggler").on("click", function() {
+$(document).ready(function () {
+  $(".menu-toggler").on("click", function () {
     $(this).toggleClass("open");
     $(".top-nav").toggleClass("open");
   });
 
-  $(".top-nav .nav-link").on("click", function() {
-    $(".").removeClass("open");
+  $(".top-nav .nav-link").on("click", function () {
+
+    //the issue you were having her is that your classes for opening that nav were not being removed
+
+    $(".menu-toggler").removeClass("open");
     $(".top-nav").removeClass("open");
   });
 
-  $('.nav a[href*="#"]').on("click", function() {
+  $('.nav a[href*="#"]').on("click", function () {
     $("html, body").animate(
-      keyframes,
-      {
+      keyframes, {
         scrollTop: $($(this).attr("href")).offset().top - 100
       },
       options,
@@ -20,19 +22,20 @@ $(document).ready(function() {
     );
   });
 
-  $("#up").on("click", function() {
-    $("html, body").animate(
-      keyframes,
-      {
-        scrollTop: 0
-      },
-      options,
-      2000
-    );
+  $("#up").on("click", function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 10);
   });
 
-  Aos.init({
+
+
+
+
+  //this issue with your aos was that you had it as Aos.init(), the aos had to be all caps
+  AOS.init({
     easing: "ease",
     duration: 1800
   });
+
 });
